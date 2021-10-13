bfbba63aa0bae97d41abde1a82faa6c3d96f2f1e
raiiiz <rickdavidsangalang@gmail.com>
2021-10-13 21:45:59 +0800

    add other operations

--- README.md
+++ README.md
@@ -1,30 +1,55 @@
 # calculator-web
 
-`Base url is /calculator
-ADD operation
+Base url is /calculator
+
+## ADD operation
+
 Uses HTTP GET
+
 Sub-url /add
+
 Send 2 arguments using via URL
+
 Returns MediaType.TEXT_PLAIN
-SUBTRACT operation
+
+## SUBTRACT operation
+
 Uses HTTP POST
+
 Sub-url /subtract
+
 Send 2 arguments using via Form
+
 Returns MediaType.TEXT_PLAIN
+
 DIVIDE operation
+
 Uses HTTP GET
+
 Sub-url /divide
+
 Send 2 arguments using via URL
+
 Returns MediaType.TEXT_PLAIN
-MULTIPLY operation
+
+## MULTIPLY operation
+
 Uses HTTP GET
+
 Sub-url /multiply
+
 Send 2 arguments using via URL
+
 Returns MediaType.TEXT_PLAIN
-SQRT operation
+
+## SQRT operation
+
 Uses HTTP GET
+
 Sub-url /sqrt
+
 Send 1 argument using via URL
+
 Returns MediaType.TEXT_PLAIN
+
 All operations will delegate to a Calculator component
-`
--- src/main/java/com/example/calculatorweb/CalculatorWebApplication.java
+++ src/main/java/com/example/calculatorweb/CalculatorWebApplication.java
@@ -3,10 +3,10 @@ package com.example.calculatorweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
 import java.util.Map;
 
@@ -17,14 +17,34 @@ public class CalculatorWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(CalculatorWebApplication.class, args);
     }
-    @GetMapping(value = "/add", produces = MediaType.TEXT_PLAIN_VALUE)
 
-    public String hello(@RequestParam Map<String,String> requestParams) {
+    @RequestMapping("/hello/")
+    public String home(HttpServletRequest request) {
+        String baseUrl = ServletUriComponentsBuilder.fromRequestUri(request)
+                .replacePath(null)
+                .build()
+                .toUriString();
+
+        System.out.println(baseUrl);
+
+        return "home";
+    }
+    @GetMapping(value = "/add", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String addition(@RequestParam Map<String,String> requestParams) {
 
         int num1=Integer.parseInt(requestParams.get("num1"));
         int num2 = Integer.parseInt(requestParams.get("num2"));
         int sum  = num1+num2;
         return "The sum of "+num1+ " and "+num2 + " is " +sum;
+    }
+
+    @PostMapping(value = "/subtract", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String substration(@RequestParam Map<String,String> requestParams) {
+
+        int num1=Integer.parseInt(requestParams.get("num1"));
+        int num2 = Integer.parseInt(requestParams.get("num2"));
+        int difference  = num1-num2;
+        return "The difference of "+num1+ " and "+num2 + " is " +difference;
     }
 
 
