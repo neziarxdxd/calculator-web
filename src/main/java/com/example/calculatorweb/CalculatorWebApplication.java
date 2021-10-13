1c94f17631f6072d6e8d6afa05a4e895bebb6101
raiiiz <rickdavidsangalang@gmail.com>
2021-10-13 12:58:32 +0800

    add two numbers

--- src/main/java/com/example/calculatorweb/CalculatorWebApplication.java
+++ src/main/java/com/example/calculatorweb/CalculatorWebApplication.java
@@ -2,6 +2,7 @@ package com.example.calculatorweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,13 +17,14 @@ public class CalculatorWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(CalculatorWebApplication.class, args);
     }
-    @GetMapping("/hello",produces = MediaType.TEXT_PLAIN_VALUE)
+    @GetMapping(value = "/add", produces = MediaType.TEXT_PLAIN_VALUE)
 
     public String hello(@RequestParam Map<String,String> requestParams) {
 
         int num1= Integer.valueOf(requestParams.get("num1"));
         int num2 =  Integer.valueOf(requestParams.get("num2"));
-        return (num1+num2).toString();
+        int sum  = num1+num2;
+        return "The sum of "+num1+ " and "+num2 + " is " +String.valueOf(sum);
     }
 
 
