19271da40114a53b14eaca65630663248fabd329
raiiiz <rickdavidsangalang@gmail.com>
2021-10-16 23:19:25 +0800

    complete functions

--- src/main/java/com/example/calculatorweb/CalculatorWebApplication.java
+++ src/main/java/com/example/calculatorweb/CalculatorWebApplication.java
@@ -12,6 +12,7 @@ import java.util.Map;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/api/")
 public class CalculatorWebApplication {
 
     public static void main(String[] args) {
@@ -21,19 +22,50 @@ public class CalculatorWebApplication {
     @GetMapping(value = "/add", produces = MediaType.TEXT_PLAIN_VALUE)
     public String addition(@RequestParam Map<String,String> requestParams) {
 
-        int num1=Integer.parseInt(requestParams.get("num1"));
-        int num2 = Integer.parseInt(requestParams.get("num2"));
-        int sum  = num1+num2;
+        double num1=Integer.parseInt(requestParams.get("num1"));
+        double num2 = Integer.parseInt(requestParams.get("num2"));
+        double sum  = num1+num2;
         return "The sum of "+num1+ " and "+num2 + " is " +sum;
     }
 
     @PostMapping(value = "/subtract", produces = MediaType.TEXT_PLAIN_VALUE)
-    public String substration(@RequestParam Map<String,String> requestParams) {
+    public String subtraction(@RequestParam Map<String,String> requestParams) {
 
-        int num1=Integer.parseInt(requestParams.get("num1"));
-        int num2 = Integer.parseInt(requestParams.get("num2"));
-        int difference  = num1-num2;
+        double num1=Integer.parseInt(requestParams.get("num1"));
+        double num2 = Integer.parseInt(requestParams.get("num2"));
+        double difference  = num1-num2;
         return "The difference of "+num1+ " and "+num2 + " is " +difference;
+    }
+
+    @GetMapping(value = "/multiply", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String multiply(@RequestParam Map<String,String> requestParams) {
+
+        double num1=Integer.parseInt(requestParams.get("num1"));
+        double num2 = Integer.parseInt(requestParams.get("num2"));
+        double product  = num1 * num2;
+        return "The product of "+num1+ " and "+num2 + " is " +product;
+    }
+    @GetMapping(value = "/divide", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String divide(@RequestParam Map<String,String> requestParams) {
+
+        double num1=Integer.parseInt(requestParams.get("num1"));
+        double num2 = Integer.parseInt(requestParams.get("num2"));
+
+        try {
+            double quotient = num1 / num2;
+            return "The quotient of " + num1 + " and " + num2 + " is " + quotient;
+        }
+        catch (Exception e){
+            return "Math error";
+        }
+    }
+    @GetMapping(value = "/divide", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String sqrt(@RequestParam Map<String,String> requestParams) {
+
+        int num=Integer.parseInt(requestParams.get("num"));
+
+        double root  = Math.sqrt(num);
+        return "The square root of " +num+" is "+root;
     }
 
 
